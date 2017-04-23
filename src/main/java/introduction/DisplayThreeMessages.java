5d2e01fc8cc665d0610dbee812b543a6128aaa22
romach <romach007@gmail.com>
2017-04-23 18:37:05 +0300

    #1: done "1.1 (Display three messages)" exercise

---
+++ src/main/java/introduction/DisplayThreeMessages.java
@@ -0,0 +1,14 @@
+package introduction;
+
+/**
+ * Class that prints three messages to the console: `Welcome to Java`, `Welcome to Computer Science`, and
+ * `Programming is fun`.
+ * @author Roman.
+ */
+public final class DisplayThreeMessages {
+    public static void main(final String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
